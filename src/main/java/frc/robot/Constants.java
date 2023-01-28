84b87eae3ba0820f26e46b9720bd76a7f3d9a042
Jelatinone <codyw2125@outlook.com>
2023-01-28 15:34:09 -0600

    New Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,9 +5,11 @@ package frc.robot;
 public final class Constants 
 {
     //SWERVE SUBSYSTEM
-    public static final int[] DRIVE_MOTORS_INDEX = {11,12,13,14};
-    public static final int[] ROTATIONAL_MOTORS_INDEX = {21,22,23,24};
-    public static final Boolean AUTOMATIC_INSTANCIZATION = true;
+    public static final Integer[] ROTATIONAL_MOTORS_INDEX = {21,22,23,24};
+    public static final Integer[] DRIVE_MOTORS_INDEX = {11,12,13,14};
+    public static final Integer[] CANCODER_INDEX = {5,6,7,8};
+    public static final Double[] CANCODER_OFFSET = {-313.682,-166.553,-246.006,-204.258};
+    public static final Boolean AUTOMATIC_INSTANCIZATION = false;
     public static final Integer FACE_COUNT = 4;
     public static final Double SS_KP = 0.1;
     public static final Double SS_KI = 0.0;
